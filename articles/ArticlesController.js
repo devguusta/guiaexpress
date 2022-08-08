adde7538b16ff4cc0846d9aecbe08b996db9b2d5
Gustavo Silva <gustavodev60@gmail.com>
2022-08-08 08:09:15 -0300

    UPDATE ARTICLE

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -66,5 +66,38 @@ router.post("/articles/delete", (req, res) => {
     }
     
 })
+router.get("/admin/articles/edit/:id", (req,res) => {
+    var id = req.params.id;
+    if(isNaN(id)){
+        res.redirect("/admin/articles");
+    }
+    Article.findByPk(id).then(article => {
+        if(article != undefined){
+        Category.findAll().then(categories => {
+
+            res.render("admin/articles/edit", {article: article, categories: categories});
+        })
+        } else {
+            res.redirect("/admin/articles");
+        }
+    }).catch(error => {
+        res.redirect("/admin/articles");
+    })
+})
+router.post("/articles/update", (req,res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    var category = req.body.category;
+
+    Category.update({title: title,  slug: slugify(title), categoryId : category}, {
+        where: {
+            id: id,
+        }
+    }).then(() => {
+        res.redirect("/admin/articles");
+    }).catch(error => {
+        console.log(error); 
+    })
+})
 
 module.exports = router;

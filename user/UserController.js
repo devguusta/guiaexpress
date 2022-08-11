8a9ded7b26157d9c48e7636f2d2204232a774664
devguusta <gustavodev60@gmail.com>
2022-08-11 08:09:11 -0300

    create user

--- user/UserController.js
+++ user/UserController.js
@@ -1,6 +1,7 @@
 const express =require('express');
 const router = express.Router();
 const User = require('./User');
+const bcrypt = require('bcryptjs');
 
 router.get("/admin/users", (req,res) => {
     res.send("Listagem de usuários");
@@ -10,10 +11,33 @@ router.get("/admin/users/create", (req, res) => {
     res.render("admin/users/create");
 })
 
-router.post("/users/create", (req, res) => {
+router.post("/users/create", async (req, res) => {
     var email = req.body.email
     var password = req.body.password;
-    res.json({email,password})
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password, salt);
+
+    var oldUser = await User.findOne({
+        where: {
+            email: email
+        },
+    })
+    if(oldUser) {
+        return res.status(400).json({
+            status: 409,
+            message: "User already exist. Please login"
+        });
+    }
+
+    User.create({
+        email: email,
+        password: hash,
+    }).then(() => {
+        res.redirect("/");
+    }).catch((err) => {
+        res.redirect("/")
+    })
+   
 
 })
 module.exports = router;
